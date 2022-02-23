6ebe12a6223a12382392db9a78f6dd2fcd7f119a
Eli <eliunited@gmail.com>
2022-02-23 13:08:11 -0800

    update bio

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,14 @@
 <div class="my-8 md:my-24">
 	<h1 class="text-3xl md:text-4xl xl:text-5xl pb-4 md:pb-8 font-bold">Eli Benton Cohen</h1>
 	<p class="text-xl md:text-3xl">
-		I'm a freelance journalist and radio producer. I am currently an associate producer at
+		I'm a freelance journalist and radio producer. I am currently reporting from the Salton Sea in
+		California. Before that, I helped produce
 		<a
 			rel="external"
 			href="https://www.wnycstudios.org/podcasts/otm"
 			class="text-blue-800 dark:text-blue-300 expand">On The Media</a
-		>. Before that I helped produce
+		>
+		and
 		<a
 			rel="external"
 			href="https://www.wnycstudios.org/podcasts/radiolab/projects/mixtape"
